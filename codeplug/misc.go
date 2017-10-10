953840e4ab94d75057dfd725a238a4ffa4cc0454
Dale Farnsworth <dale@farnsworth.org>
2017-10-10 12:17:51 -0700

    Fix overflow error for compilers with 32-bit ints

--- codeplug/misc.go
+++ codeplug/misc.go
@@ -145,12 +145,13 @@ func intToBytes(i int, len int) []byte {
 // reverse4Bytes returns an integer containing the low four bytes of
 // the input in reverse order.
 func reverse4Bytes(in int) int {
-	out := (in & 0x000000ff) << 24
-	out |= (in & 0x0000ff00) << 8
-	out |= (in & 0x00ff0000) >> 8
-	out |= (in & 0xff000000) >> 24
+	uin := uint(in)
+	out := (uin & 0x000000ff) << 24
+	out |= (uin & 0x0000ff00) << 8
+	out |= (uin & 0x00ff0000) >> 8
+	out |= (uin & 0xff000000) >> 24
 
-	return out
+	return int(out)
 }
 
 // bcdToBinary converts an BCD integer (in standard order) to a binary integer.
