DaleFarnsworth/codeplug

commit 91d93926d03a5aa16f1888f578e33841eee6a917

Download from correct original firmware URL

editcp/radio.go
@@ -413,12 +413,12 @@ with privacy laws.`
 	})
 	row.AddWidget(saveButton)
 
-	save := dialog.Exec()
-	if save {
+	saved := dialog.Exec()
+	if saved {
 		settings.europeanDB = euro
 		saveSettings()
 	}
-	return !save, download, euro
+	return !saved, download, euro
 }
 
 func firmwareDialog(title string, modelUrls []modelUrl, upgrade bool) (canceled bool, model, url string) {
@@ -428,8 +428,7 @@ func firmwareDialog(title string, modelUrls []modelUrl, upgrade bool) (canceled 
 		models[i] = modelUrl.model
 	}
 
-	model = models[0]
-	modelCombobox := ui.NewComboboxWidget(model, models, func(selected string) {
+	modelCombobox := ui.NewComboboxWidget(models[0], models, func(selected string) {
 		model = selected
 	})
 
@@ -476,6 +475,8 @@ md380 variant.`
 	})
 	row.AddWidget(saveButton)
 
+	saved := dialog.Exec()
+
 	for _, modelUrl := range modelUrls {
 		if modelUrl.model == model {
 			url = modelUrl.url
@@ -483,7 +484,7 @@ md380 variant.`
 		}
 	}
 
-	return !dialog.Exec(), model, url
+	return !saved, model, url
 }
 
 var timeoutSeconds = 20

commit 470a8788edc000b4cc5cff83bfc27178138f401f

Fix firmware URL downloaded if default is selected
This bug was introduced in commit
91d93926d03a5aa16f1888f578e33841eee6a917.

editcp/radio.go
@@ -428,7 +428,8 @@ func firmwareDialog(title string, modelUrls []modelUrl, upgrade bool) (canceled 
 		models[i] = modelUrl.model
 	}
 
-	modelCombobox := ui.NewComboboxWidget(models[0], models, func(selected string) {
+	model = models[0]
+	modelCombobox := ui.NewComboboxWidget(model, models, func(selected string) {
 		model = selected
 	})
 