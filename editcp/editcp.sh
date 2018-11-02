f7aa6d99fcc17c80a821becfdff79a6099a41c98
Dale Farnsworth <dale@farnsworth.org>
2018-11-02 01:50:55 -0700

    Turn off QT debugging on Linux

--- editcp/editcp.sh
+++ editcp/editcp.sh
@@ -12,4 +12,5 @@ export LD_LIBRARY_PATH="$dirname"/lib
 export QT_PLUGIN_PATH="$dirname"/plugins
 export QML_IMPORT_PATH="$dirname"/qml
 export QML2_IMPORT_PATH="$dirname"/qml
+export QT_LOGGING_RULES="*=false"
 "$dirname/$appname" "$@"
